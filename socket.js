6131054335018b82fd1992fbf7f6a9452f16a667
seilylook <seilylook@naver.com>
2023-02-09 13:59:39 +0900

    feat: change web socket to socket.io

--- socket.js
+++ socket.js
@@ -1,28 +1,28 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
+  const io = SocketIO(server, { path: '/socket.io' });
 
-  wss.on('connection', (ws, req) => {
+  io.on('connection', (socket) => {
+    const req = socket.request;
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log('새로운 클라이언트 접속', ip);
+    console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
 
-    ws.on('message', (message) => {
-      console.log(message.toString());
+    socket.on('disconnect', () => {
+      console.log('클라이언트 접속 해제', ip, socket.id);
+      clearInterval(socket.interval);
     });
 
-    ws.on('error', (error) => {
+    socket.on('error', (error) => {
       console.error(error);
     });
 
-    ws.on('close', () => {
-      console.log('클라이언트 접속 해제', ip);
+    socket.on('reply', (data) => {
+      console.log(data);
     });
 
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-      }
+    socket.interval = setInterval(() => {
+      socket.emit('news', 'Hello Client, Socket.io');
     }, 3000);
   });
 };
